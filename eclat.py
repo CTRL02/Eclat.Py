2d13e753f5650ecde0c0fe4be708ee2c0838fe01
CTRL02 <88464385+CTRL02@users.noreply.github.com>
2023-11-30 22:38:25 +0200

    Add files via upload

--- eclat.py
+++ eclat.py
@@ -1,24 +1,93 @@
 import pandas as pd
 from collections import defaultdict
 from collections import Counter
+from itertools import chain, combinations
 
-min_supp = 2
+min_supp = 3
 min_conf = 0.7
+rules_list = []
+strong_list = []
+lift_list = []
+
+
+# def GetLift(dataframe, items, delimit):
+#     leftItems = []
+#     for i in range(delimit):
+#         leftItems.append(items[i])
+#     result = [item for item in items if item not in leftItems]
+#     # Your formula here
+
+
+def getLift(dataframe, items):
+    num_rows = dataframe.shape[0]
+    leftr = items[0]
+    right = items[1]
+    union = float(Getsupp(dataframe, items) / num_rows)
+    result1 = float(Getsupp(dataframe, leftr) / num_rows)
+    result2 = float(Getsupp(dataframe, right) / num_rows)
+    return float(union / result1 * result2)
+
+
+def generate_subsets(items):
+    subsets = []
+    for i in range(1, len(items)):
+        subsets.extend(combinations(items, i))
+    return subsets
+
+
+def generate_association_rules(items):
+    subsets = generate_subsets(items)
+    for subset in subsets:
+        antecedent = list(subset)
+        consequent = list(set(items) - set(subset))
+        rule = (antecedent, consequent)
+        rules_list.append(rule)
 
 
 def Getsupp(dataframe, items):
+    listItem = []
+    if isinstance(items, tuple):
+        listItem = [item for sublist in items for item in sublist]
+    else:
+        listItem = items
+
+    # print(listItem)
     all_trans = []
     supp = 0
-    for item in items:
+    for item in listItem:
         for tid in range(len(dataframe)):
             if dataframe[item][tid] != 0:
                 all_trans.append(dataframe[item][tid])
     trans_counter = Counter(all_trans)
     # print(trans_counter)
     for value in trans_counter.values():
-        if value == len(items):
+        if value == len(listItem):
             supp += 1
     return supp
+
+
+def eclat(dataframe, minsup, minconf):
+    def eclat_recursive(prefix, items):
+        frequent_itemsets = []
+        for i, item in enumerate(items):
+            # print(i, item)
+            new_prefix = prefix + [item]
+            # print(new_prefix)
+            support = Getsupp(dataframe, new_prefix)
+
+            if support >= minsup:
+                frequent_itemsets.append((new_prefix, support))
+
+                next_items = items[i + 1:]
+                # print(items[i + 1:])
+                if len(next_items) > 0:
+                    frequent_itemsets.extend(eclat_recursive(new_prefix, next_items))
+
+        return frequent_itemsets
+
+    items = list(dataframe.columns)
+    frequent_itemsets = eclat_recursive([], items)
+    return frequent_itemsets
 
 
 def convert_to_vertical(dataframe):
@@ -46,8 +115,26 @@ def convert_to_vertical(dataframe):
 
 file_path = 'Horizontal_Format.xlsx'
 df = pd.read_excel(file_path, engine='openpyxl')
-print(df)
-vertical_df = convert_to_vertical(df)
-print(vertical_df)
-print("\n")
-print(Getsupp(vertical_df, ['O', 'N']))
+headers = df.columns.tolist()
+vertical_df = []
+if headers[0].lower() == "tid":
+    vertical_df = convert_to_vertical(df)
+
+frequent_items = eclat(vertical_df, min_supp, min_conf)
+for item in frequent_items:
+    if len(item[0]) >= 2:
+        generate_association_rules(item[0])
+
+for item in rules_list:
+    allItems = Getsupp(vertical_df, item)
+    left = Getsupp(vertical_df, item[0])
+    if float(allItems / left) >= min_conf:
+        strong_list.append(item)
+        lift = getLift(vertical_df, item)
+        lift_list.append(lift)
+
+print("Frequent items are:    ", frequent_items)
+print("The Association rules are: ", rules_list)
+print("The Strong rules are: ", strong_list)
+print("The associated lift values for the strong rules are: ", lift_list)
+

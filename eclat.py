354acd7185b5886783546fd7b0f365623b409c65
CTRL02 <88464385+CTRL02@users.noreply.github.com>
2023-12-01 20:40:30 +0200

    Add files via upload

--- eclat.py
+++ eclat.py
@@ -3,19 +3,17 @@ from collections import defaultdict
 from collections import Counter
 from itertools import chain, combinations
 
+# supp = input("Enter Min Support:  ")
+# conf = input("Enter Min Confidence:    ")
+# min_supp = int(supp)
+# min_conf = float(conf)
 min_supp = 3
 min_conf = 0.7
 rules_list = []
 strong_list = []
-lift_list = []
 
 
-# def GetLift(dataframe, items, delimit):
-#     leftItems = []
-#     for i in range(delimit):
-#         leftItems.append(items[i])
-#     result = [item for item in items if item not in leftItems]
-#     # Your formula here
+# all_confidence = []
 
 
 def getLift(dataframe, items):
@@ -25,9 +23,10 @@ def getLift(dataframe, items):
     union = float(Getsupp(dataframe, items) / num_rows)
     result1 = float(Getsupp(dataframe, leftr) / num_rows)
     result2 = float(Getsupp(dataframe, right) / num_rows)
-    return float(union / result1 * result2)
+    return float(union / (result1 * result2))
 
 
+# to print all possible subsets [('A',), ('B',), ('C',), ('A', 'B'), ('A', 'C'), ('B', 'C')]
 def generate_subsets(items):
     subsets = []
     for i in range(1, len(items)):
@@ -35,12 +34,13 @@ def generate_subsets(items):
     return subsets
 
 
+# take subsets and generate my association rules
 def generate_association_rules(items):
     subsets = generate_subsets(items)
     for subset in subsets:
-        antecedent = list(subset)
-        consequent = list(set(items) - set(subset))
-        rule = (antecedent, consequent)
+        atecde = list(subset)
+        conseq = list(set(items) - set(subset))
+        rule = (atecde, conseq)
         rules_list.append(rule)
 
 
@@ -54,13 +54,16 @@ def Getsupp(dataframe, items):
     # print(listItem)
     all_trans = []
     supp = 0
+    # ['a','b','c']
     for item in listItem:
+        # [(1,2,4), (1,2,3), (1,3,2)] the transactions they appeared in
         for tid in range(len(dataframe)):
             if dataframe[item][tid] != 0:
                 all_trans.append(dataframe[item][tid])
     trans_counter = Counter(all_trans)
     # print(trans_counter)
     for value in trans_counter.values():
+        # if transaction id counter same as length of an item then they appeared together in this transaction
         if value == len(listItem):
             supp += 1
     return supp
@@ -70,16 +73,13 @@ def eclat(dataframe, minsup, minconf):
     def eclat_recursive(prefix, items):
         frequent_itemsets = []
         for i, item in enumerate(items):
-            # print(i, item)
             new_prefix = prefix + [item]
-            # print(new_prefix)
             support = Getsupp(dataframe, new_prefix)
 
             if support >= minsup:
                 frequent_itemsets.append((new_prefix, support))
 
                 next_items = items[i + 1:]
-                # print(items[i + 1:])
                 if len(next_items) > 0:
                     frequent_itemsets.extend(eclat_recursive(new_prefix, next_items))
 
@@ -93,11 +93,14 @@ def eclat(dataframe, minsup, minconf):
 def convert_to_vertical(dataframe):
     inverted_index = defaultdict(list)
     for index, row in dataframe.iterrows():
+        # tid 1
         transaction_id = row.iloc[0]
+        # monkey
         items = row.iloc[1:]
         for item in items:
             if pd.notna(item):
                 item = str(item).strip()
+                #get each letter from monkey
                 letters = [letter for letter in item if letter.strip() and letter != ',']
                 for letter in letters:
                     found = False
@@ -119,22 +122,40 @@ headers = df.columns.tolist()
 vertical_df = []
 if headers[0].lower() == "tid":
     vertical_df = convert_to_vertical(df)
+else:
+    vertical_df = df
 
+# print("Our vertical data:   ")
+# print(vertical_df)
 frequent_items = eclat(vertical_df, min_supp, min_conf)
+print(frequent_items)
+# print("Our frequent items:   ")
+# print(frequent_items)
 for item in frequent_items:
     if len(item[0]) >= 2:
         generate_association_rules(item[0])
 
+# print("Our Association rules are:   ")
+# print(rules_list)
+# for item in rules_list:
+#     allItems = Getsupp(vertical_df, item)
+#     left = Getsupp(vertical_df, item[0])
+#     print(item, ": its confidence value:  ", float(allItems / left))
+#     # all_confidence.append(float(allItems / left))
+#     if float(allItems / left) >= min_conf:
+#         strong_list.append(item)
+
+# print("The Strong rules are: ", strong_list)
+new_list = []
 for item in rules_list:
-    allItems = Getsupp(vertical_df, item)
-    left = Getsupp(vertical_df, item[0])
-    if float(allItems / left) >= min_conf:
-        strong_list.append(item)
-        lift = getLift(vertical_df, item)
-        lift_list.append(lift)
-
-print("Frequent items are:    ", frequent_items)
-print("The Association rules are: ", rules_list)
-print("The Strong rules are: ", strong_list)
-print("The associated lift values for the strong rules are: ", lift_list)
-
+    reversed_item = (item[1], item[0])
+    if reversed_item not in new_list:
+        new_list.append(item)
+#
+# for item in new_list:
+#     if getLift(vertical_df, item) > 1:
+#         print(item, "is dependent and +ve correlated with lift value:    ", getLift(vertical_df, item))
+#     elif getLift(vertical_df, item) < 1:
+#         print(item, "is dependent and -ve correlated with lift value:    ", getLift(vertical_df, item))
+#     else:
+#         print(item, "is independent with lift value:    ", getLift(vertical_df, item))
